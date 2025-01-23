4301e9fbbb412d0718ff1d0dbc7d3305544eecdb
Margoburrito <rchistyakova@gmail.com>
2025-01-23 13:28:44 +0300

    added choosing browser and assert text of button

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as firefoxOptions
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 
 language_list = {"ar", "ca", "cs", "da", "de", "en-gb", "el", "es", "fi", "fr", "it", "ko", "nl", "pl", "pt", "pt-br", "ro", "ru", "sk", "uk", "zh-hans"}
 
@@ -8,19 +10,39 @@ language_list = {"ar", "ca", "cs", "da", "de", "en-gb", "el", "es", "fi", "fr", 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en-gb',
                      help="Choose language")
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     user_language = request.config.getoption("language")
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        if user_language in language_list:
+            print(f"\nchosen language is {user_language}")
+            options = Options()
+            options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+            browser = webdriver.Chrome(options=options)
+        else:
+            raise pytest.UsageError("selected language is not supported")
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        if user_language in language_list:
+            print(f"\nchosen language is {user_language}")
+            options = firefoxOptions()
+            firefox_profile = FirefoxProfile()
+            firefox_profile.set_preference("intl.accept_languages", user_language)
+            options.profile = firefox_profile
+            browser = webdriver.Firefox(options=options)
+        else:
+            raise pytest.UsageError("selected language is not supported")
 
-    if user_language in language_list:
-        print(f"\nstart chrome browser for test, chosen language is {user_language}")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        browser = webdriver.Chrome(options=options)
     else:
-        raise pytest.UsageError("selected language is not supported")
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
     yield browser
     print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,14 +1,28 @@
 import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+button_languages = dict({'ar': 'أضف الى سلة التسوق', 'ca': 'Afegeix a la cistella',
+             "cs": 'Vložit do košíku', "da": 'Læg i kurv',
+             'de': 'In Warenkorb legen', "en-gb": 'Add to basket',
+             "el": 'Προσθήκη στο καλάθι', "es": 'Añadir al carrito',
+             "fi": 'Lisää koriin', "fr": 'Ajouter au panier',
+             "it": 'Aggiungi al carrello', "ko": '장바구니 담기',
+             "nl": 'Voeg aan winkelmand toe', "pl": 'Dodaj do koszyka',
+             "pt": 'Adicionar ao carrinho', "pt-br": 'Adicionar à cesta',
+             "ro": 'Adauga in cos', "ru": 'Добавить в корзину',
+             "sk": 'Pridať do košíka',"uk": 'Додати в кошик',
+             "zh-hans": 'Add to basket'})
+print(type(button_languages))
 
 def test_add_button(browser):
     browser.get(link)
     button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert button.is_displayed() is True
-    assert button.text == 'Añadir al carrito'
 
+    chosen_language = browser.find_element(By.CSS_SELECTOR, '[selected="selected"]')
+    text_button = button_languages[chosen_language.get_attribute('value')]
+    assert button.text == text_button
 
-
+#//*[@id="language_selector"]/div/select/option[8]
